e80530f9be8a36a79c87cb30b7e99523864bf0c1
pritimunde1999 <pritimunde0109@gmail.com>
2023-09-21 14:21:54 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,54 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderBallOrButton.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+const App = () => {
+
+    const [renderBall,setRenderBall] = useState(false);
+    
+    const [ballPosition,setBallPosition] = useState({left:0,top:0});
+    
+
+    function move(l,t){
+        setBallPosition({
+            left: ballPosition.left+1,
+            top: ballPosition.top+1
+        })
+    }
+
+    const handleListener = (event) =>{
+
+        switch(event.keycode){
+            case 39: 
+            move(5,0);
+            break;
+        }
+
+    }
+
+    useEffect(()=> {
+        document.addEventListener("keydown",handleListener);
+        return () => document.removeEventListener("keydown",handleListener);
+    },[ballPosition]);
+
+
+     const buttonClickHandler = () => {
+        setRenderBall(true);
+   };
+    const renderBallOrButton = () => {
+		if (renderBall) {
+		    return <div className="ball" style={{
+                left : ballPosition.left +"px",
+                top : ballPosition.top + "px",
+                position: "absolute",
+            }}></div>
+		} else {
+		    return <button className="start" onClick={buttonClickHandler} >Click For One Ball</button>
+		}
     };
 
-    buttonClickHandler() {
    
-   }
-    renderBallOrButton() {
-		if (this.state.renderBall) {
-		    return <div className="ball" style={this.state.ballPosition}></div>
-		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
-		}
-    }
-
-    // bind ArrowRight keydown event
-    componentDidMount() {
-      
-    }
-
-    render() {
-        return (
-            <div className="playground">
-                {this.renderBallOrButton()}
-            </div>
-        )
-    }
+   
+    return  <div className="playground"> {renderBallOrButton()}</div>;
 }
 
 
